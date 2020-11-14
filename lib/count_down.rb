eaee3aba96b2c7c0ddb101020ea72d95a1ff3ca6
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-11-14 16:50:10 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,9 +1,9 @@
 # Write your code here
 counter = 11
-exit_number = 0
 
-while counter > 0
+until counter = 0
   counter -= 1
-  break if counter == exit_number
-  puts "Happy New Year!"
+  puts counter
 end
+
+puts "Happy New Year!"
